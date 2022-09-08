50e0c4ddf0bd38212f2e080d90b724ec2ffd9497
joseong9 <jsg8825@gamil.com>
2022-09-08 16:40:37 +0900

    'save;

--- 03.JavaScript/ch05.함수/07.재귀함수.js
+++ 03.JavaScript/ch05.함수/07.재귀함수.js
@@ -11,3 +11,12 @@ function factorial(n) {
     return n* factorial(n-1)
 }
 console.log(`10!은 ${factorial(10)}입니다`)
+
+//fibonacci
+function fibo(n) {
+    if (n == 0 || n == 1)
+        return 1;
+    return fibo(n-1) + fibo(n-2);
+}
+for (let i=0; i<10; i++)
+    console.log(fibo(i))

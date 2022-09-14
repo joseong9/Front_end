4702b3e2795a5324eb4d19c5aacf677a4118cf5b
joseong9 <jsg8825@gamil.com>
2022-09-14 14:13:10 +0900

    'save'

--- 03.JavaScript/ch07.표준내장객체/06.ES.js
+++ 03.JavaScript/ch07.표준내장객체/06.ES.js
@@ -12,4 +12,23 @@ const fruits1 = ['grape', 'mango'];
 const furits2 = [fruits, fruits1];
 console.log(furits2);
 const fruits3 = [...fruits, ...fruits1];
-console.log(fruits3)
+console.log(fruits3);
+
+const numbers = [4, 16, 9, 24, 56];
+console.log(Math.max(numbers));
+console.log(Math.max(...numbers))
+
+//3.let, const
+//4.template literals
+//5.3항 연산자
+//6.화살표 함수
+//7.default parameters
+//8.object shorthand property
+//9.class
+//10.set
+const mySet = new Set()
+mySet.add(1);
+mySet.add(3);
+mySet.add(1);
+console.log(mySet);
+console.log(mySet.had(3));

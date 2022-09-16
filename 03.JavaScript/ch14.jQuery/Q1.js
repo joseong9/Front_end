ee7ef63250878d1ae87a206c6bcab3cf290fa41f
joseong9 <jsg8825@gamil.com>
2022-09-16 17:32:42 +0900

    jQuery

--- 03.JavaScript/ch14.jQuery/Q1.js
+++ 03.JavaScript/ch14.jQuery/Q1.js
@@ -1,6 +1,12 @@
+$(document).ready(function() {
+    $('#answer-q1').hide();
+    $('#answer-q2').hide();
+    $('#answer-q3').hide();
+});
+
 function answer1() {
-    const first = parseInt(document.getElementById('first').value);
-    const second = parseInt(document.getElementById('second').value);
+    const first = $('#first').val();
+    const second = $('#second').val();
     console.log(first, second);
     let result = [];
     for (let i=first; i<=second; i++) {
@@ -8,11 +14,11 @@ function answer1() {
             continue;
         result.push(Math.pow(2, i));
     }
-    document.getElementById('result1').innerHTML = result.join(', ');
+    $('#result1').html(result.join(', '));
 }
 
 function answer2() {
-    const text = document.getElementById('text').value;
+    const text = $('#text').val();
     const noPunct = text.replace(/[-',.]/g, '').replace(/\n/g, ' ');    // 구둣점 제거, 줄바꿈은 공백으로 변경
     console.log(noPunct);
     const words = noPunct.split(' ');
@@ -21,31 +27,38 @@ function answer2() {
         if (word == 'the')
             count++;
     }
-    document.getElementById('result2').innerHTML = `the의 갯수는 <strong>${count}</strong>개`;
+    $('#result2').html(`the의 갯수는 <strong>${count}</strong>개`);
 }
 
 function answer3() {
-    const num1 = parseInt(document.getElementById('num1').value);
-    const num2 = parseInt(document.getElementById('num2').value);
+    const num1 = parseInt($('#num1').val());
+    const num2 = parseInt($('#num2').val());
+
+    console.log(num1);
+    console.log(num2);
+
     const divisors1 = [];
     for (let i=1; i<=num1; i++) {
         if (num1 % i == 0)
         divisors1.push(i);          // num1의 약수
     }
+
+    console.log(divisors1);
     const divisors2 = [];
     for (let i=1; i<=num2; i++) {
         if (num2 % i == 0)
             divisors2.push(i);      // num2의 약수
     }
+    console.log(divisors2);
     const commonDivisors = divisors1.filter(val => divisors2.includes(val));
-    document.getElementById('result3').innerHTML = `Set(${commonDivisors.join(', ')})`;
+    console.log(commonDivisors);
+    $('#result3').html(`Set(${commonDivisors.join(', ')})`);
 }
+function event1(obj,type){
 
-function event1(){
-    alert('333');
-    // if(type == 'hide'){
-    //     $('#answer-q'+obj).hide();
-    // }else{
-    //     $('#answer-q'+obj).show();
-    // }
+    if(type == 'hide'){
+        $('#answer-q'+obj).hide();
+    }else{
+        $('#answer-q'+obj).show();
+    }
 }
